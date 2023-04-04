ff6854a74e02e2f53fd3ba7365fe1c50b6bdc977
seoyeonK <hasunnyha@gmail.com>
2023-04-04 14:16:26 +0900

    [python]IO_practice_230404.py

---
+++ IO/[python]IO_practice.py
@@ -0,0 +1,15 @@
+# import sys
+
+# N,M = map(int,sys.stdin.readline().split(" "))
+
+# cubes = []
+
+# for i in range(N):
+#     cubes.append(list(map(int,sys.stdin.readline().rstrip())))
+
+# print(cubes)
+
+var_X = True
+
+if var_X:
+    print(1)
