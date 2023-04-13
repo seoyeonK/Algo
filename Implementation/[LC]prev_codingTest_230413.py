4d407fd7cc8a19605f217408111c6fd58ddce2a1
seoyeonK <hasunnyha@gmail.com>
2023-04-13 23:50:59 +0900

    [LC] prev coding test review_230413

---
+++ Implementation/[LC]prev_codingTest_230413.py
@@ -0,0 +1,33 @@
+def balancedSum(arr):
+    lsum = arr[0]
+    pivot = 1
+    rsum = sum(arr) - lsum - arr[pivot]
+    for i in range(pivot,len(arr)-2):
+        if rsum == lsum :
+            return pivot
+        lsum = + arr[pivot]
+        rsum -= arr[pivot+1]
+        pivot += 1
+    return pivot
+
+
+def squares(queries):
+    total = []
+    for q in queries:
+        r = q[0]
+        c = q[1]
+        if r > c :
+            r,c = c,r
+        count = int( r*(r+1)*(2*r+1)/6 + (c-r)*r*(r+1)/2   )
+    total.append(count)
+    return total
+    
+
+def findLIS(nums):
+    n = len(nums)
+    rst = [1] *n
+    for i in range(1,n):
+        for j in range(i):
+            if nums[i] > nums[j]:
+                rst[i] = max(rst[i], rst[j]+1)
+    return max(rst)
