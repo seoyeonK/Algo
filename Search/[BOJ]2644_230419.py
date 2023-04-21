77a72e49fd309b1bf91c9961f48a055b261f9908
seoyeonK <hasunnyha@gmail.com>
2023-04-21 17:54:03 +0900

    [BOJ]2644_230419.py

--- Search/[BOJ]2644_230419.py
+++ Search/[BOJ]2644_230419.py
@@ -1,28 +1,64 @@
-w, h = map(int,input().split())
-num = int(input())
+N = int(input())
+A, B = map(int, input().split())
+M = int(input())
 
-X = [0,w]
-Y = [0,h]
-for _ in range(num):
-    x, y = map(int, input().split())
-    if x == 0:
-        Y.append(y)
-    else:
-        X.append(y)
+fam = [[] for _ in range(N+1)]
 
-X.sort()
-Y.sort()
+for _ in range(M):
+    P, C = map(int,input().split())
+    fam[P].append(C)
 
-wid = []
-hei = []
+def findmy(child):
+    parent = [child]
+    for i in range(1,N+1):
+        if child in fam[i]:
+            parent += findmy(i)
+    return parent
 
-for i in range(len(X)-1):
-    wid.append(X[i+1]-X[i])
+pA = findmy(A)
+pB = findmy(B)
 
-for i in range(len(Y)-1):
-    hei.append(Y[i+1]-Y[i])
+if pA[-1] == pB[-1]: #같은 가좍
+    pA_ = pA
+    pA = set(pA) - set(pB)
+    pB = set(pB) - set(pA_)
+    print(len(pA)+len(pB))
+else:
+    print(-1)
 
-wid = sorted(list(set(wid)))
-hei = sorted(list(set(hei)))
 
-print(wid[-1]*hei[-1])
+
+
+# REF
+# # 입력값 받는 부분
+# N = int(input())
+# A, B = map(int, input().split())
+# M = int(input())
+# graph = [[] for _ in range(N+1)]
+# visited = [False] * (N+1)
+# result = []
+# ####
+
+# # 어떤 노드들이 연결되어 있는지 graph라는 2차원 배열에 저장
+# for _ in range(M):
+#   x, y = map(int, input().split())  
+#   graph[x].append(y)
+#   graph[y].append(x)
+
+# # dfs
+# def dfs(v, num):
+#   num += 1
+#   visited[v] = True
+
+#   if v == B:
+#     result.append(num)
+
+#   for i in graph[v]:
+#     if not visited[i]:
+#       dfs(i, num)
+
+# dfs(A, 0)
+# if len(result) == 0:
+#   print(-1)
+# else:
+#   print(result[0]-1)
