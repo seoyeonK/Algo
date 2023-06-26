790af4d70df0fe05d88edbb980b1e51be53d5311
seoyeonK <hasunnyha@gmail.com>
2023-06-26 19:35:58 +0900

    [BOJ]2231_230626.py

--- BruteForce/[BOJ]2231_230617.py
+++ BruteForce/[BOJ]2231_230617.py
@@ -1,16 +1,21 @@
-N = int(input())
+N = int(input())     # 1 ≤ N ≤ 1,000,000
 
 answer = 0
 
-digits = len(list(map(int,str(N))))
+digits = len(list(map(int,str(N)))) 
 
+#N = 9이면
+start = N - 9*digits
+end = N+1  #10
 
-for i in range(N//2, N-1):
-    gen = i
+if start < 0:
+    start = N // 2
+
+for gen in range(start,end): # 0 - 9
     if N == gen + sum( list(map(int,str(gen))) ):
         answer = gen
         break
 
 print(answer)
 
-# 시간 줄이기
+# 시간 줄이기!!
