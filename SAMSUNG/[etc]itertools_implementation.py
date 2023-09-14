17b9bc5e48a3ac57ec98e2009f1fb7d2ad3db7c6
seoyeonK <hasunnyha@gmail.com>
2023-09-14 12:08:44 +0900

    [etc] itertools implementation

---
+++ SAMSUNG/[etc]itertools_implementation.py
@@ -0,0 +1,44 @@
+import itertools
+
+# 1-1. itertools.permutations
+test2 = ['A', 'B', 'C']
+
+permutations_test = list(map(''.join, itertools.combinations(test2,2)))
+
+
+
+# 1-2. permutations implementation
+def permutations(arr, r):
+    for i in range(len(arr)):
+        if r == 1:
+            yield [arr[i]]
+        else:
+            for next in permutations(arr[:i] + arr[i+1:], r-1):
+                yield [arr[i]] + next
+                
+                
+                
+                
+                
+# 2-1. itertools.combinations
+test1 = ['A', 'B', 'C']
+
+combination_test = list(map(''.join, itertools.combinations(test1,2)))
+
+
+# 2-2. combinations implementation
+
+def combinations(arr, r):
+    for i in range(len(arr)):
+        if r == 1:
+            yield [arr[i]]
+        else:
+            for next in combinations(arr[i+1:], r-1):
+                yield [arr[i]] + next
+       
+       
+       
+        
+
+                
+#https://velog.io/@yeseolee/python%EC%9C%BC%EB%A1%9C-%EC%88%9C%EC%97%B4%EA%B3%BC-%EC%A1%B0%ED%95%A9-%EC%A7%81%EC%A0%91-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
